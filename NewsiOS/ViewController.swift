ca61064bb659f38887aa3d71a49ffe56ad002920
Karen <madoyan.kar@gmail.com>
2021-01-25 21:44:29 +0400

    Initial Commit

---
+++ NewsiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsiOS
+//
+//  Created by MAC on 2021/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
